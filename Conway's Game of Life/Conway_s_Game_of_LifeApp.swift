edd8ad0f2806e2f9071271a6c6e1e784a3c664ac
LuckDuracell <lukedru@yahoo.com>
2021-11-27 20:21:08 -0800

    Initial Commit

---
+++ Conway's Game of Life/Conway_s_Game_of_LifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Conway_s_Game_of_LifeApp.swift
+//  Conway's Game of Life
+//
+//  Created by Luke Drushell on 11/27/21.
+//
+
+import SwiftUI
+
+@main
+struct Conway_s_Game_of_LifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
